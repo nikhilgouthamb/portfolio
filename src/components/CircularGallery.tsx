b7af6c58f332535b42aa9d361787d328db7cc344
Nikhil Goutham <bnikhilgoutham@gmail.com>
2025-06-08 13:05:15 -0400

    cg

--- src/components/CircularGallery.tsx
+++ src/components/CircularGallery.tsx
@@ -13,9 +13,9 @@ import "./CircularGallery.css";
 
 type GL = Renderer["gl"];
 
-function debounce<T extends (...args: any[]) => void>(func: T, wait: number) {
+function debounce<T extends (...args: unknown[]) => void>(func: T, wait: number) {
   let timeout: number;
-  return function (this: any, ...args: Parameters<T>) {
+  return function (this: unknown, ...args: Parameters<T>) {
     window.clearTimeout(timeout);
     timeout = window.setTimeout(() => func.apply(this, args), wait);
   };
@@ -25,11 +25,11 @@ function lerp(p1: number, p2: number, t: number): number {
   return p1 + (p2 - p1) * t;
 }
 
-function autoBind(instance: any): void {
+function autoBind(instance: object): void {
   const proto = Object.getPrototypeOf(instance);
   Object.getOwnPropertyNames(proto).forEach((key) => {
-    if (key !== "constructor" && typeof instance[key] === "function") {
-      instance[key] = instance[key].bind(instance);
+    if (key !== "constructor" && typeof (instance as Record<string, unknown>)[key] === "function") {
+      (instance as Record<string, unknown>)[key] = ((instance as Record<string, unknown>)[key] as Function).bind(instance);
     }
   });
 }
@@ -468,7 +468,7 @@ class App {
     last: number;
     position?: number;
   };
-  onCheckDebounce: (...args: any[]) => void;
+  onCheckDebounce: ReturnType<typeof debounce>;
   renderer!: Renderer;
   gl!: GL;
   camera!: Camera;
